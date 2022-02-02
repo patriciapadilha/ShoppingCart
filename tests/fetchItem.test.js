17a7d3b19b490d3c8ae0af9acbde4f3cd1d857b6
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-02-02 20:36:55 -0300

    dev: Requisito 9 - testes da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -10,8 +10,20 @@ describe('2 - Teste a função fecthItem', () => {
   it('Verifica se a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada;', async () => {
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled();
-  })
+  });
   it("Verifica se ao chamar a função fetchItem com o argumento do item 'MLB1615760527', a função fetch utiliza o endpoint 'https://api.mercadolibre.com/items/MLB1615760527'", async () => {
-
+    const endPoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+  it('Verifica se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+  it('Verifica se ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const result = await fetchItem();
+    expect(result).toEqual(new Error ('You must provide an url'));
   })
+
+
 });
